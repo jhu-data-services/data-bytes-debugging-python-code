73fec0e585d83dc048bc497f159790c4b8ce0bf8
Pete Lawson <geekypete@gmail.com>
2022-10-03 07:54:06 -0400

    added for loop decrementing

--- In-ClassScripts/debugging_python.py
+++ In-ClassScripts/debugging_python.py
@@ -1,4 +1,10 @@
-def divide(numerator, denominator):
-    quotient = numerator/denominator
-    return(f'quotient is {quotient:.2f}')
-    
+def func1(a, b):
+    return a / b
+
+def func2(x):
+    a = x
+    b = x - 1
+    return func1(a, b)
+
+for val in range(5, 1, -1):
+    func2(val)
